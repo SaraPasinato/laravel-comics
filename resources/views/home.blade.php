8b0e1544f9082a830a449dcddbec9d9b92434070
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-01 17:01:01 +0200

    add main layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset("css/app.css")}}">
-    <title>Main</title>
-</head>
-<body>
-    <!-- >>creare un includes header -->
-   @include('includes.header')
-    <main>
-        <!-- TODO >>creare un layout jumbotron -->
-        <section id='jumbotron'>
-            <div class="container-sm">
-                <p><a href="#" class="btn btn-primary-full">Current Series</a></p>
-            </div>
-        </section>
-    </main>
-    <script src="{{asset('js/app.js')}}"></script>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('content')
+<div class="container-sm">
+    <p><a href="#" class="btn btn-primary-full">Current Series</a></p>
+</div>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset("css/app.css")}}">
+    <title>Main</title>
+</head>
+<body>
+    <!-- >>creare un includes header -->
+   @include('includes.header')
+    <main>
+        <!-- TODO >>creare un layout jumbotron -->
+        <section id='jumbotron'>
+            <div class="container-sm">
+               @yield('content')
+            </div>
+        </section>
+    </main>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
