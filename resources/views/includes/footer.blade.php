bae7fdd11dde52d44b627e999767180c647fa8aa
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-03 16:25:46 +0200

    add footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,5 @@
 <footer>
+    {{-- footer links  --}}
 <section id="footer-links" class="">
   <div class="container row">
           <div class="col-6 mt-3">
@@ -53,4 +54,23 @@
       </div>
   </div>
   </section>
+  {{-- footer socials --}}
+  <section id="socials">
+    <div class="row container">
+        <div class="col-6">
+          <a href="#" class="btn btn-primary">Sign-up now</a>
+        </div>
+        <div class="col-6">
+            <div id="items" class="row">
+                <h3>Follow us</h3>
+                <a href="" class=""><img src="{{asset('images/footer-facebook.png')}}" alt="fb"></a>
+                <a href="" class="btn-rounded "><img src="{{asset('images/footer-twitter.png')}}" alt="twitter"></a>
+                <a href="" class=""><img src="{{asset('images/footer-youtube.png')}}" alt="yt"></a>
+                <a href="" class=""><img src="{{asset('images/footer-pinterest.png')}}" alt="pinterest"></a>
+                <a href="" class=""><img src="{{asset('images/footer-periscope.png')}}" alt="periscope"></a>
+              
+            </div>
+        </div>
+    </div>
+</section> 
 </footer>
