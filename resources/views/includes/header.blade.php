19ba779cba60c4f507d985060d1ce88c0b2f870c
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-03 18:17:52 +0200

    add animation

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,16 +6,16 @@
         </div>
         <nav class="col">
           <ul class="list-nav">
-            <li><a href="{{route('home')}}">Comics</a></li>
-            <li><a href="{{route('details')}}">Details</a></li>
-            <li><a href="#">movies</a></li>
-            <li><a href="#">tv</a></li>
-            <li><a href="#">games</a></li>
-            <li><a href="#">collections</a></li>
-            <li><a href="#">videos</a></li>
-            <li><a href="#">fans</a></li>
-            <li><a href="#">news</a></li>
-            <li><a href="#">shop</a></li>
+            <li><a href="{{route('home')}}" class="{{ request()->routeIs('home') ? 'active' :' '}}">Comics</a></li>
+            <li><a href="{{route('details')}}" class="{{ request()->routeIs('details') ? 'active' :' '}}">Details</a></li>
+            <li><a href="#" class="{{ request()->routeIs('movies') ? 'active' :' '}}">movies</a></li>
+            <li><a href="#" class="{{ request()->routeIs('tv') ? 'active' :' '}}">tv</a></li>
+            <li><a href="#" class="{{ request()->routeIs('games') ? 'active' :' '}}">games</a></li>
+            <li><a href="#" class="{{ request()->routeIs('collections') ? 'active' :' '}}">collections</a></li>
+            <li><a href="#" class="{{ request()->routeIs('videos') ? 'active' :' '}}">videos</a></li>
+            <li><a href="#" class="{{ request()->routeIs('fans') ? 'active' :' '}}">fans</a></li>
+            <li><a href="#" class="{{ request()->routeIs('news') ? 'active' :' '}}">news</a></li>
+            <li><a href="#" class="{{ request()->routeIs('shop') ? 'active' :' '}}">shop</a></li>
           </ul>
         </nav>
       </div>
